941dfbcf123e3e7139826858b82e05b30a31c382
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-10 19:52:26 +0000

    Made head.js to get first element of array. file currently has tests contained

---
+++ head.js
@@ -0,0 +1,13 @@
+function head(array) {
+  return array[0];
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(
+    `✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(
+    `⛑️⛑️⛑️ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual(head([,1,1]), undefined);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
