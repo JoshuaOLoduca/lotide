2ac5381ccc9451849e01749bbc6bdb8e2080d225
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-11 20:48:12 +0000

    Added new function that compares arrays

---
+++ eqArrays.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(
+    `✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(
+    `⛑️⛑️⛑️ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(firstList, secondList) {
+  let areSame = false;
+
+  if (areSameLength(firstList, secondList)) {
+    areSame = haveSameContents(firstList, secondList);
+  }
+  return areSame;
+};
+
+const haveSameContents = function(firstList, secondList) {
+  let listLength = firstList.length;
+  let areSame = false;
+
+  for (let i = 0; i <= listLength; i++) {
+    if (firstList[i] !== secondList[i]) {
+      areSame = false;
+      break;
+    } else areSame = true;
+  }
+
+  return areSame;
+};
+
+
+const areSameLength = function(firstItem, secondItem) {
+  let sameLength = true;
+  if (firstItem.length !== secondItem.length) sameLength =  false;
+
+  return sameLength;
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+
+// Failed
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+
+// Failed
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
