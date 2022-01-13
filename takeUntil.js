c2632b112fd7f9f1e4e5568afbdbaa1e04d2f277
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-13 19:12:11 +0000

    added takeUntil method for arrays

---
+++ takeUntil.js
@@ -0,0 +1,67 @@
+const takeUntil = function(array, callback) {
+  let slice = [];
+
+  for (let item of array) {
+    if (callback(item)) break;
+    slice.push(item);
+  }
+
+  return slice;
+}
+const assertArraysEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqArrays(actual, expected)) console.log(
+    `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  else console.log(
+    `⛑️⛑️⛑️ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+};
+
+const eqArrays = function(firstList, secondList) {
+  let areSame = false;
+
+  if (areSameLength(firstList, secondList)) {
+    areSame = haveSameContents(firstList, secondList);
+  }
+  return areSame;
+};
+
+const haveSameContents = function(firstList, secondList) {
+  let listLength = firstList.length;
+  let areSame = false;
+
+  for (let i = 0; i <= listLength; i++) {
+    if (firstList[i] !== secondList[i]) {
+      areSame = false;
+      break;
+    } else areSame = true;
+  }
+
+  return areSame;
+};
+
+
+const areSameLength = function(firstItem, secondItem) {
+  let sameLength = true;
+  if (firstItem.length !== secondItem.length) sameLength =  false;
+
+  return sameLength;
+};
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
+
+// TEST CODE
+assertArraysEqual(results1,[1,2,5,7,2]); // => pass
+assertArraysEqual(results2,["I've", "been", "to", "Hollywood"]); // => pass
+
+
