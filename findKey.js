b0253d48a915f71bee18ab7c0a374ba6d189573e
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-13 19:20:26 +0000

    added findKey function

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) return key;
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(
+    `✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(
+    `⛑️⛑️⛑️ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3); // => "Akaleri"
+
+assertEqual(result1, 'noma');
+assertEqual(result2, 'Akaleri');
