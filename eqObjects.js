0f06194fa0fc1ae7bc0fc77bbde8d591fea29e2e
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-15 00:19:21 +0000

    test cases for eqObjects with nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -87,15 +87,25 @@ const assertEqual = function(actual, expected) {
     `⛑️⛑️⛑️ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true); // => true
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false); // => false
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+console.log(eqObjects(
+  { a: { z: 1 }, b: 2 },
+  { a: { z: 1 }, b: 2 })) // => true
+
+console.log(eqObjects(
+  { a: { y: 0, z: 1 }, b: 2 },
+  { a: { z: 1}, b: 2 })) // => false
+console.log(eqObjects(
+  { a: { y: 0, z: 1 }, b: 2 }, 
+  { a: 1, b: 2 })) // => false
