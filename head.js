9772f2f6bae6ab3c6081e60204777042c39a9309
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-17 18:14:16 +0000

    Refact: assertEqual and head to be DRY

--- head.js
+++ head.js
@@ -1,13 +1,7 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(array) {
   return array[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log(
-    `✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  else console.log(
-    `⛑️⛑️⛑️ Assertion Failed: ${actual} !== ${expected}`);
-};
-
-assertEqual(head([,1,1]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
