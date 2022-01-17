ef5d934b28701d0a88e2bce33a2b4f5c264d37a3
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-17 20:39:10 +0000

    exported letterPositions to lotide.js

--- lotide.js
+++ lotide.js
@@ -2,10 +2,12 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
 
 module.exports = {
   head,
   tail,
   middle,
-  countLetters
+  countLetters,
+  letterPositions
 };
