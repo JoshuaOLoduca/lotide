0a6c56d66afe122d975fca034923d359bdb8c1f6
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-17 18:48:21 +0000

    converted headTest to use Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,24 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const { expect } = require('chai');
+const head   = require('../head');
 
-// Test code
-assertEqual(head([,1,1]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    const input = [1, 2, 3];
+    const expected = 1;
+    expect(head(input)).to.equal(expected);
+  });
+
+  it("returns '5' for ['5']", () => {
+    const input = ['5'];
+    const expected = '5';
+    expect(head(input)).to.equal(expected);
+  });
+
+  it("returns undefined for [,1,1]", () => {
+    const input = [,1,1];
+    const expected = undefined;
+    expect(head(input)).to.equal(expected);
+  });
+
+});
